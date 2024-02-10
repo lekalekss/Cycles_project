fc11759712d675150e1232afd78bc705cc23fa49
lekalekss <155861509+lekalekss@users.noreply.github.com>
2024-02-10 17:16:05 -0600

    new bottle

--- Cycles project/Program.cs
+++ Cycles project/Program.cs
@@ -6,75 +6,103 @@ namespace Cycles_project
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 10; i++)
-            {
-                if (i % 2 == 0)
-                    Console.WriteLine(i + " is even number");
-                else
-                    Console.WriteLine(i + " is odd number");
-            }
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i + " is even number");
+            //    else
+            //        Console.WriteLine(i + " is odd number");
+            //}
 
-            int count = 1;
+            //int count = 1;
 
-            while (count <= 10)
-            {
+            //while (count <= 10)
+            //{
 
-                if (count % 3 == 0)
-                    Console.WriteLine(count + " is multiple of 3");
-                else
-                    Console.WriteLine(count + " is non-multiple of 3");
-                count++;
-            }
+            //    if (count % 3 == 0)
+            //        Console.WriteLine(count + " is multiple of 3");
+            //    else
+            //        Console.WriteLine(count + " is non-multiple of 3");
+            //    count++;
+            //}
 
-            int guess = 0;
-            bool guessnum = false;
-            do
-            {
-                Console.WriteLine("enter the number");
-                string number = Console.ReadLine();
-                int num = int.Parse(number);
+            //int guess = 0;
+            //bool guessnum = false;
+            //do
+            //{
+            //    Console.WriteLine("enter the number");
+            //    string number = Console.ReadLine();
+            //    int num = int.Parse(number);
 
-                if (num < 5)
-                {
-                    Console.WriteLine("guessed number lower");
-                    continue;
-                }
+            //    if (num < 5)
+            //    {
+            //        Console.WriteLine("guessed number lower");
+            //        continue;
+            //    }
 
-                else if (num > 5)
-                {
-                    Console.WriteLine("guessed number higher");
-                    continue;
-                }
+            //    else if (num > 5)
+            //    {
+            //        Console.WriteLine("guessed number higher");
+            //        continue;
+            //    }
 
-                else if (num == 5)
-                {
-                    guessnum = true;
-                    Console.WriteLine("guessed right!!!!");
-                    break;
-                }
+            //    else if (num == 5)
+            //    {
+            //        guessnum = true;
+            //        Console.WriteLine("guessed right!!!!");
+            //        break;
+            //    }
 
-            }
-            while (!guessnum);
-
-
-            int[] numbers = new int[] { 10, 20, 30, 40, 50 };
-
-            foreach (int number in numbers)
-            {
-                if (number > 30)
-                    Console.WriteLine(number + " number more than 30");
-                else if (number < 30)
-                    Console.WriteLine(number + " number lower than 30");
-                else if (number == 30)
-                    Console.WriteLine(number + " number equal 30");
-
-            }
+            //}
+            //while (!guessnum);
 
 
+            //int[] numbers = new int[] { 10, 20, 30, 40, 50 };
 
+            //foreach (int number in numbers)
+            //{
+            //    if (number > 30)
+            //        Console.WriteLine(number + " number more than 30");
+            //    else if (number < 30)
+            //        Console.WriteLine(number + " number lower than 30");
+            //    else if (number == 30)
+            //        Console.WriteLine(number + " number equal 30");
+
+            //}
+
+
+            Console.WriteLine("Welcome in the store");
+            Console.WriteLine("We have new car");
             
+            Car suv = new Car(4);
+            suv.CarClass = "SUV";
+            suv.IsCabriolet = false;
+            suv.NumberOfDoors = 4;
 
 
+
+            Console.WriteLine("this car " + suv.CarClass);
+            Console.WriteLine("Is it cabriolet?  " + suv.IsCabriolet);
+            Console.WriteLine("It has doors " + suv.NumberOfDoors);
+
+            suv.StartEngine();
+            suv.Drive();
+            suv.StopEngine();
+
+            Console.WriteLine("We have one more car");
+
+            Car cabrio = new Car(2);
+
+            cabrio.IsCabriolet = true;
+            cabrio.NumberOfDoors = 2;
+            cabrio.CarClass = "Coupe";
+
+            Console.WriteLine("this car " + cabrio.CarClass);
+            Console.WriteLine("Is it cabriolet?  " + cabrio.IsCabriolet);
+            Console.WriteLine("It has doors " + cabrio.NumberOfDoors);
+
+            cabrio.StartEngine();
+            cabrio.Drive();
 
 
         }
---
+++ Cycles project/bottle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycles_project
+{
+    public class Bottle
+    {
+        public int Volume {  get; set; }
+        public string Material { get; set; }
+        public string FormFactor { get; set; }
+
+        public void Fall()
+        {
+            Console.WriteLine("Bottle fall down");
+        }
+
+        public void Decomposed() 
+        {
+            Console.WriteLine("bottle decomposed");
+        }
+
+    }
+    public class Cup
+    {
+        int Volume { get; set; }
+        string Material { get; set; }
+        string Color { get; set; }
+
+        void Break()
+        {
+            Console.WriteLine("Cup has brokken");
+        }
+
+        void FillUp()
+        {
+            Console.WriteLine("Fullfilled the cup");
+        }
+
+
+    }
+
+    public class Car
+    {
+        public Car(int numberOfDoors)
+        {
+            NumberOfDoors = numberOfDoors;
+        }
+
+
+
+
+        public string CarClass;
+        public int NumberOfDoors { get; set; }
+        public bool IsCabriolet { get; set; }
+
+        public void StartEngine()
+        {
+            Console.WriteLine("Vroom Vroom");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine("Engine turned off");
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving");
+        }
+
+
+    }
+}
